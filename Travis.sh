280ecc68d718702538c67fdd286b602f7254c3ed
Denis Lebedev <lebedevdg@ekassir.com>
2020-03-03 20:44:42 +0300

    Update Travis.sh

--- Travis.sh
+++ Travis.sh
@@ -1,4 +1,27 @@
-#!/bin/bash
+#!/bin/bash -x
+
+set +e
+
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+DARKYELLOW="\033[0;33m"
+CYAN="\033[0;36m"
+LIGHTGRAY="\033[0;37m"
+
+BRANCH="${TRAVIS_BRANCH:-master}"
+BRANCH_TO_TEST="${BRANCH_TO_TEST:-master}"
+# debug:
+#BRANCH_TO_TEST=$BRANCH
+
+if [ "$BRANCH" != "$BRANCH_TO_TEST" ]
+then
+  echo -e "${DARKYELLOW}Nothing to do for ${BRANCH} branch${LIGHTGRAY}"
+  exit 0
+fi
+
+sudo apt-get update -qq > /dev/null
+sudo apt-get upgrade -yqq > /dev/null
+sudo apt-get install -yqq tree curl unzip grep > /dev/null
 
 PACKER_VERSION=1.5.4
 TERRAFORM_VERSION=0.12.21
